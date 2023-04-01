a65c06dea094fffcf322e9432ac3f683e69c1ebd
Nguyễn Hữu Nhân <82103041+NguyenHuuNhan1912@users.noreply.github.com>
2023-04-01 07:40:13 -0500

    Add files via upload

---
+++ client/vite.config.js
@@ -0,0 +1,23 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  server: {
+    port: 1010,
+    proxy: {
+      "/api": {
+        target: "http://localhost:1011/",
+        changeOrigin: true,
+      }
+    }
+  },
+})
